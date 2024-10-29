afbeb9ad2251b70b36756679eba48ccea007ada6
Victor <victor.teillaud@ensea.fr>
2024-10-29 15:12:31 +0100

    création d'une branche SQL

--- routes/users.js
+++ routes/users.js
@@ -1,17 +1,18 @@
 const express = require("express")
 const router = express.Router()
+const db = require("../database")
 
-const usersArray = [
-    { id: 1, firstName: 'John', lastName: 'Doe', role: 'admin' },
-	{ id: 2, firstName: 'Jane', lastName: 'Smith', role: 'user' },
-	{ id: 3, firstName: 'Alice', lastName: 'Johnson', role: 'moderator' },
-	{ id: 4, firstName: 'Bob', lastName: 'Brown', role: 'user' },
-	{ id: 5, firstName: 'Charlie', lastName: 'Davis', role: 'admin' }
 
-]
 
 router.get("/users", (req, res) => {
-    res.json(usersArray)
+    db.all('SELECT * FROM users', [], (err, rows) => {
+		if (err) {
+		  res.status(500).json({ error: err.message });
+		} else {
+		  res.json(rows);
+		}
+	  });
+	
 })
 
 // GET : LIRE Un seul utilisateur
