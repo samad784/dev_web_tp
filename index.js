1a60f01d2d31b95600ad075417e3167ab29ba375
Victor <victor.teillaud@ensea.fr>
2024-10-29 13:31:18 +0100

    suite de la partie 2

--- index.js
+++ index.js
@@ -8,95 +8,13 @@ app.use(express.json());
 //USER Endpoint
 app.use("/api/", usersRouter)
 
-
+// GET : HOME GET METHOD
+app.get("/", (req, res) => {
+    res.json({
+        msg: "Welcome to my users API",
+    })
+})
 
 app.listen(port, () => {
 	console.log(`Serveur en cours d'exécution sur http://localhost:${port}`);
-});
-
-const users = [
-	{ id: 1, firstName: 'John', lastName: 'Doe', role: 'admin' },
-	{ id: 2, firstName: 'Jane', lastName: 'Smith', role: 'user' },
-	{ id: 3, firstName: 'Alice', lastName: 'Johnson', role: 'moderator' },
-	{ id: 4, firstName: 'Bob', lastName: 'Brown', role: 'user' },
-	{ id: 5, firstName: 'Charlie', lastName: 'Davis', role: 'admin' }
-];
-
-
-
-// GET : LIRE Un seul utilisateur
-app.get("/:id", (req, res) => {
-	const id = parseInt(req.params.id)
-
-	// trouve son index, verifier si le userIndex est positive
-	const userIndex = users.findIndex((user) => user.id === id)
-
-	// utilisateur non trouvé
-	if (userIndex < 0)
-		return res.status(404).json({ msg: "utilisateur non trouvé" })
-    // si el est trouvé
-
-	res.json(users[userIndex])
-})
-
-// GET : LIRE tous les utilisateurs
-app.get("/", (req, res) => {
-	res.json(users)
-})
-
-// POST : CRÉER un nouvel utilisateur, basé sur les données passées dans le corps(body) de la requête
-app.post("/", (req, res) => {
-	// récupérer toutes les données qui arrivent dans le corps de la requête (body)
-	const { firstName, lastName } = req.body
-
-	// récupérer l'ID du dernier utilisateur en fonction du nombre d'utilisateurs dans notre variable de tableau 'users'.
-	const lastId = users[users.length - 1].id
-	// ajouter un pour créer un utilisateur unique
-	const newId = lastId + 1
-
-	// créer le nouvel utilisateur avec les données du corps de la requête et l'ID calculé
-	const newUser = {
-		firstName,
-		lastName,
-		id: newId,
-	}
-
-	// ajouter le nouvel utilisateur à notre liste d'utilisateurs en utilisant la méthode 'push'
-	users.push(newUser)
-	// envoyer le code de statut 201 (créé) et les données du nouvel utilisateur afin de confirmer au client.
-	res.status(201).json(newUser)
-})
-// PUT : Modifier un utilisateur en fonction de son ID
-app.put("/:id", (req, res) => {
-    // récupérer toutes les données qui arrivent dans le corps de la requête (body)
-	const {firstName, lastName} = req.body
-    // récupère l'id dans la requête et le transforme en int
-    const id = parseInt(req.params.id)
-    // trouve son index, verifier si le userIndex est positive
-	const userIndex = users.findIndex((user) => user.id === id)
-    // utilisateur non trouvé
-	if (userIndex < 0)
-		return res.status(404).json({ msg: "utilisateur non trouvé" })
-    // si il est trouvé, nous vérifions quelles valeurs ont été envoyées
-	if (firstName) users[userIndex].firstName = firstName
-	if (lastName) users[userIndex].lastName = lastName
-        res.json({
-            msg: "utilisateur mis à jour",
-            user: users[userIndex],
-        })
-})
-
-app.delete("/:id", (req, res) => {
-	const id = parseInt(req.params.id)// trouve son index, verifier si le userIndex est positive
-	const userIndex = users.findIndex((user) => user.id === id)
-
-	// utilisateur non trouvé
-	if (userIndex < 0)
-		return res.status(404).json({ msg: "utilisateur non trouvé" })
-    // si el est trouvé
-	users.splice(userIndex, 1)
-
-	res.json({
-		msg: "utilisateur suprimée",
-	})
 })
