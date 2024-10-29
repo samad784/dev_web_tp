70262693350daa460da831f9608dfa192a429206
Victor <victor.teillaud@ensea.fr>
2024-10-29 14:11:19 +0100

    création db

--- index.js
+++ index.js
@@ -4,31 +4,6 @@ const port = 3000;
 const usersRouter = require("./routes/users.js")
 const sqlite3 = require("sqlite3").verbose()
 
-// Open the database connection
-const db = new sqlite3.Database("./users.db", (err) => {
-	if (err) {
-		console.error("Error opening database:", err.message)
-	} else {
-		console.log("Connected to the SQLite database.")
-
-		// Create the items table if it doesn't exist
-		db.run(
-			`CREATE TABLE IF NOT EXISTS users (
-        id INTEGER PRIMARY KEY AUTOINCREMENT,
-        firstName TEXT NOT NULL,
-        lastName TEXT NOT NULL
-      )`,
-			(err) => {
-				if (err) {
-					console.error("Error creating table:", err.message)
-				}
-			}
-		)
-	}
-})
-
-module.exports = db
-
 //MIDDLEWARE
 app.use(express.json());
 //USER Endpoint
