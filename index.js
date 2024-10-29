4546acf1746ec34282df108d12b2e3c758923a0e
Victor <victor.teillaud@ensea.fr>
2024-10-29 13:32:10 +0100

    encore

--- index.js
+++ index.js
@@ -16,5 +16,5 @@ app.get("/", (req, res) => {
 })
 
 app.listen(port, () => {
-	console.log(`Serveur en cours d'exécution sur http://localhost:${port}`);
+	console.log(`Serveur en cours d'exécution sur http://localhost:${port}`)
 })
